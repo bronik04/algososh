9e169abec15ba0395ae90a592cecd9ba743d4e3f
Nikolay Brovkin <bronik004@gmail.com>
2023-02-18 18:37:30 +0300

    feat: added string layout

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./string.module.css";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
 
 export const StringComponent: React.FC = () => {
-  return (
-    <SolutionLayout title="Строка">
-     
-    </SolutionLayout>
-  );
+    return (
+        <SolutionLayout title="Строка">
+            <div className={styles.container}>
+                <Input
+                    placeholder="Введите строку"
+                    extraClass={styles.input}
+                    isLimitText={true}
+                    maxLength={11}
+                />
+                <Button text={"Развернуть"}/>
+            </div>
+
+        </SolutionLayout>
+    );
 };
