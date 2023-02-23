f5f7aa81740ca9e8e9539ed7494cc3cad57c7cea
Nikolay Brovkin <bronik004@gmail.com>
2023-02-23 23:10:47 +0300

    stack: added layout and styles

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./stack.module.css";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
 
 export const StackPage: React.FC = () => {
   return (
     <SolutionLayout title="Стек">
-
+        <form className={styles.form}>
+            <div className={styles.container}>
+                <Input
+                    extraClass={styles.input}
+                    isLimitText={true}
+                    maxLength={4}
+                />
+                <Button type="submit" text="Добавить"/>
+                <Button type="submit" text="Удалить"/>
+            </div>
+            <Button type="submit" text="Очистить"/>
+        </form>
     </SolutionLayout>
   );
 };
