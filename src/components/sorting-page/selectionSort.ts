2c63f166f4104e066e08a2906f628a3bf49450e6
Nikolay Brovkin <bronik004@gmail.com>
2023-03-01 22:10:58 +0300

    sort:  added selectionSortDesc

--- src/components/sorting-page/selectionSort.ts
+++ src/components/sorting-page/selectionSort.ts
@@ -30,3 +30,29 @@ export const selectionSortAsc = async (
     setArray([...arr]);
     setActive(false);
 }
+
+export const selectionSortDesc = async (
+    arr: TArrayItem[],
+    setArray: Dispatch<React.SetStateAction<TArrayItem[]>>,
+    setActive: Dispatch<SetStateAction<boolean>>) => {
+    setActive(true);
+    for (let i = 0; i < arr.length - 1; i++) {
+        let maxIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            arr[i].state = ElementStates.Changing;
+            arr[j].state = ElementStates.Changing;
+            setArray([...arr]);
+            await delay(SHORT_DELAY_IN_MS);
+            if (arr[j].item > arr[maxIndex].item) {
+                maxIndex = j;
+            }
+            arr[j].state = ElementStates.Default;
+            setArray([...arr]);
+        }
+        swap(arr, i, maxIndex);
+        arr[i].state = ElementStates.Modified;
+    }
+    arr[arr.length - 1].state = ElementStates.Modified;
+    setArray([...arr]);
+    setActive(false);
+}
