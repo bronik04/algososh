1e2219c20858b9c00c0f10afbfa6391280b09ab2
Nikolay Brovkin <bronik004@gmail.com>
2023-03-01 13:18:14 +0300

    sort: added sorting-page layout

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,44 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {RadioInput} from "../ui/radio-input/radio-input";
+import styles from "./sorting-page.module.css";
+import {Button} from "../ui/button/button";
+import {Column} from "../ui/column/column";
 
 export const SortingPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Сортировка массива">
+    return (
+        <SolutionLayout
+            title="Сортировка массива"
+        >
+            <div className={styles.wrapper}>
+                <div className={styles.container}>
+                    <div className={styles.container__radio}>
+                        <RadioInput label={"Выбор"}/>
+                        <RadioInput label={"Пузырёк"}/>
+                    </div>
 
-    </SolutionLayout>
-  );
+                    <div className={styles.container__btn}>
+                        <Button
+                            text={"По возрастанию"}
+                            extraClass={styles.btn}
+                        />
+                        <Button
+                            text={"По убыванию"}
+                            extraClass={styles.btn}
+                        />
+                        <Button
+                            text={"Новый массив"}
+                            extraClass={styles.btn}
+                        />
+                    </div>
+                </div>
+                <div className={styles.column__container}>
+                    <Column
+                        index={10}
+                        extraClass={styles.column}
+                    />
+                </div>
+            </div>
+        </SolutionLayout>
+    );
 };
